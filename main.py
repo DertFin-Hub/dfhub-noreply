2287d20170e65d81e7b2053c22cd0368ca882262
Dertfin3051 <dertfin@gmail.com>
2025-01-18 15:39:48 +0300

    Add configuration

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import smtplib
+
 import dotenv
 from flask import Flask
+import json
 
-dotenv.load_dotenv()
-
-app = Flask("dfhub-noreply")
+app: Flask = Flask("")
+config: dict = {}
+DEBUG = False
 
 @app.route("/")
 def index():
     return "DFHub-NoReply active"
 
-app.run()
+def load_config():
+    global config
+    with open("config.json", 'r', encoding="utf-8") as config_file:
+        config = json.loads(config_file.read())
+
+    if not "public-address" in config:
+        config["public-address"] = "127.0.0.1"
+    if not "port" in config:
+        config["port"] = 587
+
+    if config["public-address"] in ["127.0.0.1", "localhost"]:
+        print(f"WARNING! Server running on {config['public-address']} and can't be accessed from external network!")
+
+if __name__ == "__main__":
+    print("Loading .env file")
+    dotenv.load_dotenv()
+    print("Initializing Flask server")
+    app = Flask("dfhub-uptime")
+    print("Reading config.json")
+    load_config()
+
+    app.run(
+        config["public-address"],
+        config["port"],
+        DEBUG
+    )
