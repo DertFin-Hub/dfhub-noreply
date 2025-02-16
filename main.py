624f79296f0dd945043872a738937828b1b19e22
dertfin0 <dertfin@gmail.com>
2025-02-16 20:37:40 +0300

    Reopen SMTP connection for every request

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ from requests.exceptions import InvalidHeader
 app: Flask = Flask("dfhub-uptime")
 config: dict = {}
 DEBUG = False
-smtp = smtplib.SMTP()
 
 @app.route("/")
 def index():
@@ -41,7 +40,7 @@ def send_raw():
         abort(400, description="JSON param \"context\" is not specified!")
 
     try:
-        smtp.sendmail(os.getenv("DFHUB_NOREPLY_APP_MAIL"), json_body["to"], f"Subject: {json_body['subject']}\n\n{json_body['context']}".encode("utf-8"))
+        get_smtp().sendmail(os.getenv("DFHUB_NOREPLY_APP_MAIL"), json_body["to"], f"Subject: {json_body['subject']}\n\n{json_body['context']}".encode("utf-8"))
         return "Message request sent!"
     except SMTPException:
         return abort(500, "An internal error occurred while sending a message")
@@ -71,7 +70,7 @@ def send_html_url():
         abort(500, "Cannot get the specified resource (attachment_url)!")
     attachment = MIMEText(html, "html")
     msg.attach(attachment)
-    smtp.send_message(msg)
+    get_smtp().send_message(msg)
 
     return "Message request sent!"
 
@@ -96,7 +95,7 @@ def send_html():
 
     attachment = MIMEText(json_body["context"], "html")
     msg.attach(attachment)
-    smtp.send_message(msg)
+    get_smtp().send_message(msg)
 
     return "Message request sent!"
 
@@ -120,28 +119,30 @@ def is_accessed_ip(ip: str) -> bool:
     """Check is IP address in accessed IPs list"""
     return ip in config["accessed-ips"]
 
-def init_smtp() -> bool:
-    global smtp
+def get_smtp():
     global config
     """
     Initialize SMTP-server connection
-    :return: Status (success/error)
+    :return: New SMTP Connection
     """
+
+    smtp = smtplib.SMTP()
+
     if os.getenv("DFHUB_NOREPLY_APP_MAIL") in [None, ""]:
         print("Environment \"DFHUB_NOREPLY_APP_MAIL\" is not specified!")
-        return False
+        return None
 
     if os.getenv("DFHUB_NOREPLY_APP_TOKEN") in [None, ""]:
         print("Environment \"DFHUB_NOREPLY_APP_TOKEN\" is not specified!")
-        return False
+        return None
 
     if not "smtp-address" in config or config["smtp-address"] == "":
         print("Config param \"smtp-address\" is not specified!")
-        return False
+        return None
 
     if not "smtp-port" in config:
         print("Config param \"smtp-port\" is not specified!")
-        return False
+        return None
 
     try:
         if config["is-ssl"]:
@@ -153,15 +154,15 @@ def init_smtp() -> bool:
             smtp.starttls()
     except SMTPException:
         print("Can't connect to SMTP server")
-        return False
+        return None
 
     try:
         smtp.login(os.getenv("DFHUB_NOREPLY_APP_MAIL"), os.getenv("DFHUB_NOREPLY_APP_TOKEN"))
     except SMTPException:
         print("The SMTP server rejected authorization. The information may be incorrect")
-        return False
+        return None
 
-    return True
+    return smtp
 
 if __name__ == "__main__":
     print("Loading .env file")
@@ -171,7 +172,7 @@ if __name__ == "__main__":
     load_config()
 
     print("Connecting to SMTP server...")
-    if not init_smtp():
+    if get_smtp() is None:
         sys.exit(0)
 
     print("Initializing Flask server")
